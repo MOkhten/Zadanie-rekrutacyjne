4ccbc0e6888255809cdb58fde05743662b921936
MOkhten <m.okhten@gmail.com>
2023-08-15 22:58:06 +0300

    header styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'lg': {'max' :'1262px'},
+      'md': {'max': '768px'},
+      'sm': {'max': '480px'}
+    },
+    container: {
+      padding: '20px',
+    },
+    extend: {
+      colors: {
+        beige: '#DCC1AB',
+        black: '#111111',
+        green: '#1B5B31',
+        grey: '#F5F0EC',
+        white: '#FFFFFF'
+
+      }
+    },
   },
   plugins: [],
 }
